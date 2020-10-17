f73487bb86ab52a9173dbcc2ec8a77acbf74f33b
leandro barroso <lrbarroso1984@gmail.com>
2020-10-17 16:30:59 -0300

    ExcangeRate model created and tested

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,10 @@
 class Currency < ApplicationRecord
-  validates :code, :symbol, :name, :country, :flag, presence: true
+  has_many :currency_from_exchanges, class_name: 'ExchangeRate', foreign_key: 'currency_from_id'
+  has_many :currency_to_exchanges, class_name: 'ExchangeRate', foreign_key: 'currency_to_id'
+
+  validates :code, :name, :country, :flag, presence: true
+  validates :code, length: { is: 3 }
+  validates :symbol, length: { maximum: 3 }
+  validates :country, length: { in: 3..20 }
+  validates :flag, length: { in: 1..20 }
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 
 RSpec.describe Currency, type: :model do
   # Validation tests
-  # ensure columns name, age, gender, location and infected are present before saving
+  # ensure columns code, symbol, name, country and flag are present before saving
   it { should validate_presence_of(:code) }
-  it { should validate_presence_of(:symbol) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:country) }
   it { should validate_presence_of(:flag) }

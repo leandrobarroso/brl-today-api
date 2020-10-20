136ea42f612118820eddf6cadd372f9025c96341
leandro barroso <lrbarroso1984@gmail.com>
2020-10-20 20:26:42 -0300

    Curencies table seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'seeding DB...'
+puts 'creating currencies'
+
+data = [
+  {
+    code: 'BRL',
+    symbol: 'R$',
+    name: 'Brazilian Real',
+    country: 'Brazil',
+    flag: '&#127463;&#127479;'
+  },
+  {
+    code: 'USD',
+    symbol: '$',
+    name: 'US Dollar',
+    country: 'USA',
+    flag: '&#127482;&#127480;'
+  },
+  {
+    code: 'EUR',
+    symbol: '€',
+    name: 'Euro',
+    country: 'European Union',
+    flag: '&#127466;&#127482;'
+  },
+  {
+    code: 'JPY',
+    symbol: '¥',
+    name: 'Japanese Yen',
+    country: 'Japan',
+    flag: '&#127471;&#127477;'
+  },
+  {
+    code: 'GBP',
+    symbol: '£',
+    name: 'Pounds Sterling',
+    country: 'United Kingdom',
+    flag: '&#127468;&#127463;'
+  },
+  {
+    code: 'AUD',
+    symbol: 'A$',
+    name: 'Australian Dollar',
+    country: 'Australia',
+    flag: '&#127462;&#127482;'
+  },
+  {
+    code: 'CAD',
+    symbol: 'C$',
+    name: 'Canadian Dollar',
+    country: 'Canada',
+    flag: '&#127464;&#127462;'
+  }
+]
+
+data.each do |attributes|
+  currency = Currency.new(
+    code: attributes[:code],
+    symbol: attributes[:symbol],
+    name: attributes[:name],
+    country: attributes[:country],
+    flag: attributes[:flag]
+  )
+  currency.save!
+  puts currency.name
+end
+puts 'currencies created!'
+puts 'done'
